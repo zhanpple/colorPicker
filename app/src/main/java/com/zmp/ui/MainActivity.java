5204860dd805851470f9faf560784c71ba61cc95
zhangmingping <mingping.zhang@uurobot.cn>
2020-04-22 14:50:24 +0800

    添加SimpleCircleProgress

--- app/src/main/java/com/zmp/ui/MainActivity.java
+++ app/src/main/java/com/zmp/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.zmp.ui;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.zmp.widget.view.SimpleCircleProgress;
 
 /**
  * Created by zmp on 2020/4/21 10:20
@@ -16,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.main_act);
+                setContentView(R.layout.main_act2);
+                SimpleCircleProgress circleProgress = (SimpleCircleProgress) findViewById(R.id.scp);
+                circleProgress.setProgressColors(Color.RED, Color.GREEN, Color.YELLOW,Color.RED);
         }
 
 }
